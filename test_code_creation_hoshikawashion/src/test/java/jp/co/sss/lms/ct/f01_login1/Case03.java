03a742487c7929bfb5a13c7c8036e5417bd55b36
hoshikawa <hoshikawa@TEC-PC-027>
2026-02-24 10:55:45 +0900

    fix：Case03.java、evidence  ログイン正常系テスト
    ・プロジェクト名の変更をするために、一回GitHubの母体を削除したため新しく作成。
    
    ・Case1を実施した後、初回ログイン済みの受講生ユーザーでログインし、コース詳細画面に遷移するテスト。
    ・それに伴い「evidenceファイル/各種エビデンス」と「docファイル/各種テスト仕様書」を作成及び取得。

--- test_code_creation_hoshikawashion/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_hoshikawashion/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +38,42 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// 画面遷移
+		goTo("http://localhost:8080/lms/");
+		// ログイン画面の中に「"loginId"というIDを持つ要素が画面に表示されているか」
+		assertTrue(webDriver.findElement(By.id("loginId")).isDisplayed());
+		// エビデンス取得
+		getEvidence(new Case01() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログイン画面を開く
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		WebElement pass = webDriver.findElement(By.id("password"));
+		
+		// テストを毎回同じ条件で実行するためにclearメソッドを実行
+		loginId.clear();
+		pass.clear();
+		
+		// 初回ログイン済みユーザーID・パスワードを入力
+		loginId.sendKeys("StudentAA01");
+		pass.sendKeys("StudentAA011");
+		
+		// ログインボタンを押す
+		WebElement loginButton = webDriver.findElement(By.className("btn-primary"));
+		loginButton.click();
+		
+		// コース詳細画面に遷移することを検証する
+		WebElement courseDetail = webDriver.findElement(By.className("active"));
+		assertEquals("コース詳細", courseDetail.getText());
+		
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
