b02f1d4c40a754cd469e0a92903f3014b3b04399
hoshikawa <hoshikawa@TEC-PC-027>
2026-02-25 15:33:24 +0900

    fix：Case04.java、evidence、doc  よくある質問画面への遷移
    Test01 トップページURLでアクセスし、ログイン画面への遷移後、"loginId"というIDを持つ要素が画面に表示されているか確認する。
    
    Test02 初回ログイン済みの受講生ユーザーでログインし、ログインボタンを押下後、
    コース詳細画面に遷移することを確認する。
    （この時に、パスワードが短すぎたり簡単すぎるとポップアップが出てきてTest03でエラーが出てしまう）
    
    Test03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移するか確認する。
    
    Test04 「よくある質問」リンクからよくある質問画面を別タブに開くか確認する。
    
    ・上記に伴い「evidenceファイル/各種エビデンス」と「docファイル/各種テスト仕様書」を作成及び取得。

--- test_code_creation_hoshikawashion/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_hoshikawashion/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -85,50 +85,21 @@ public class Case04 {
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		
+		// 要素の描画遅延を考慮し、最大10秒間待機する
 		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-		// コース詳細画面に機能というプルダウン（ドロップダウンメニュー）があるか探す。
-//		WebElement dropdown = webDriver.findElement(By.className("dropdown-toggle"));
-//		
-//		// クリックする
-//		dropdown.click();
 		
-		// そのプルダウンに ヘルプ リンクがあるか探す
-//		WebElement function = webDriver.findElement(By.className("dropdown-toggle"));
-		
+		// クラス名"dropdown"の要素を探し、その要素がクリック可能になるまで待つ
 		WebElement function = wait.until(
 				ExpectedConditions.elementToBeClickable(By.className("dropdown"))
 		);
 		// クリックする
 		function.click();
-		
-//		// エビデンス取得
-//		getEvidence(new Object() {
-//		});
-		
-		// org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {"method":"partial link text","selector":"ヘルプ"}
-		// このように赤タグが出てきたので、どこで間違えているかのチェックを実施
-//		System.out.println("URL: " + webDriver.getCurrentUrl());
-//		System.out.println(webDriver.getTitle());
-//		System.out.println("ヘルプ含まれている？: " +webDriver.getPageSource().contains("ヘルプ"));
-//		WebElement helpLink = webDriver.findElement(By.cssSelector("a[href*='/lms/help']"));
 
-		
-		// そのプルダウンに ヘルプ リンクがあるか探す
-//		WebElement helpLink = webDriver.findElement(By.partialLinkText("ヘルプ"));
-//		WebElement helpLink = webDriver.findElement(By.cssSelector("a[href='http://localhost:8080/lms/help']"));
-//		WebElement helpLink = webDriver.findElement(By.xpath("//a[contains(@href,'/lms/help')]"));
-//		System.out.println("Displayed: " + helpLink.isDisplayed());  要素が画面上に見えているか
-//		System.out.println("Enabled: " + helpLink.isEnabled());  要素が操作可能か
-
+		// ヘルプリンク押下
 		WebElement helpLink = wait.until(
 				ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/lms/help']"))
 		);
-		// クリックする
 		helpLink.click();
-		
-		// 本当にヘルプ画面に遷移したか確認
-//		assertEquals("http://localhost:8080/lms/help", webDriver.getCurrentUrl());
 		
 		// エビデンス取得
 		getEvidence(new Object() {
@@ -139,39 +110,27 @@ public class Case04 {
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// Case3で遷移した画面の中にある「よくある質問」を探す
+		// 「よくある質問」リンクを取得
 		WebElement faq = webDriver.findElement(By.linkText("よくある質問"));
-		
-		// クリックする
 		faq.click();
 		
-		// getWindowHandle()は「現在操作中のタブを識別するID」を返すメソッドで、
-		// このIDはブラウザ内部でユニークな文字列として管理されてるため「型はString」となる
+		// 現在のタブハンドルを保持
 		String tab = webDriver.getWindowHandle();
 		
 		// 新しいタブを開く
 		WebElement newTab = webDriver.findElement(By.id("wrap"));
-		
-		// クリックする
 		newTab.click();
 		
-		// 全てのタブのIDを取得
-		// allTabs の中に「元のタブ+新しいタブ」が含まれる
-		// 元のタブのハンドルと違うものを探す → 新しいタブがどれか分かる
+		// 全タブを取得し、新規タブへ切り替え
 		Set<String> allTabs = webDriver.getWindowHandles();
-		
-		// 現在いくつのタブが開かれているか分からないから拡張for文で全て見て、
-		// その中のtab（現在操作中のタブを識別するID）じゃないのがあったら
-		// （新しいタブを）操作対象にする  ※ 全ハンドルを取得しないと、新しいタブを識別できない
 		for(String nowTab : allTabs) {
 			if(!nowTab.equals(tab)) {
-				// 新しいタブを操作対象にする
 				webDriver.switchTo().window(nowTab);
 				break;
 			}
 		}
 		
-		// 本当によくある質問画面に遷移したか確認
+		// FAQ画面へ正しく遷移したことを確認
 		assertEquals("http://localhost:8080/lms/faq", webDriver.getCurrentUrl());
 		
 		// エビデンス取得
